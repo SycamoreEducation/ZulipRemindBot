f31f514eb6f1e70abaf8d4cb4b1887806fe3fc38
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2018-08-28 13:25:21 -0500

    Handle formatting issues with help message

--- app/controllers/zulip_remind_bot/controller.rb
+++ app/controllers/zulip_remind_bot/controller.rb
@@ -10,14 +10,19 @@ module ZulipRemindBot
       Time.zone = ZulipRemindBot.tz
       Chronic.time_class = Time.zone
 
-      to = data['message']['sender_email']
+      @to = data['message']['sender_email']
       parts = data['message']['content'].split('|')
+
+      return unparseable if parts.count != 2
+
       message = parts.first.strip
       time_str = parts.second.strip
       time_str = "today #{time_str}" if time_str =~ /^at/
       time = Chronic.parse(time_str)
 
-      RemindersJob.set(wait_until: time).perform_later(to, message)
+      return unparseable if time.nil?
+
+      RemindersJob.set(wait_until: time).perform_later(@to, message)
 
       human_time = time.strftime('%A, %b %e at %l:%M%p')
 
@@ -25,5 +30,16 @@ module ZulipRemindBot
 
       head :no_content
     end
+
+    private
+      def unparseable
+        ZulipService.send(@to, <<~STR)
+          Your request could not be parsed. Here are some valid examples:
+          * `go home | at 5pm`
+          * `have lunch | thursday at noon`
+          * `do something | in 5 minutes`
+        STR
+        head :accepted
+      end
   end
 end
